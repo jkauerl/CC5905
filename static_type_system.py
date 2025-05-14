16b0ecff947412da65fe1b2cf6265953905769df
Javier Kauer <kauer.javier01@gmail.com>
2025-05-14 19:14:58 -0400

    Feat: implementation of set join and meet

--- static_type_system.py
+++ static_type_system.py
@@ -100,6 +100,75 @@ def upper_set(psi: Psi, ti: ClassName) -> set[ClassName]:
     return {T for T in psi.Ns if is_subtype(psi, ti, T)}
 
 
+def meet(psi: Psi, ti: ClassName, tj: ClassName) -> set[ClassName]:
+    """Return the meet (greatest lower bound) of two class names in the type system.
+
+    :param psi: The Psi object representing the type system.
+    :param ti: The first class name.
+    :param tj: The second class name.
+    :return: A set of ClassName representing the meet.
+    """
+    common = lower_set(psi, ti).intersection(lower_set(psi, tj))
+
+    meet_set = set()
+    for T in common:
+        if all(not is_subtype(psi, T, T_prime) for T_prime in common if T != T_prime):
+            meet_set.add(T)
+
+    return meet_set
+
+
+def meet_unique(psi: Psi, ti: ClassName, tj: ClassName) -> ClassName | None:
+    """Return the unique meet of two class names in the type system.
+    
+    :param psi: The Psi object representing the type system.
+    :param ti: The first class name.
+    :param tj: The second class name.
+    :return: The unique meet of the two class names.
+    """
+    meet_set = meet(psi, ti, tj)
+    if len(meet_set) == 1:
+        return next(iter(meet_set))
+    else:
+        return None
+
+
+def join(psi: Psi, ti: ClassName, tj: ClassName) -> set[ClassName]:
+    """Return the join (least upper bound) of two class names in the type system.
+
+    The join is the set of minimal elements in the intersection of the higher sets
+    of `ti` and `tj`.
+
+    :param psi: The Psi object representing the type system.
+    :param ti: The first class name.
+    :param tj: The second class name.
+    :return: A set of ClassName representing the join.
+    """
+    common = upper_set(psi, ti).intersection(upper_set(psi, tj))
+
+    join_set = set()
+    for T in common:
+        if all(not is_subtype(psi, T_prime, T) for T_prime in common if T != T_prime):
+            join_set.add(T)
+
+    return join_set
+
+
+def join_unique(psi: Psi, ti: ClassName, tj: ClassName) -> ClassName | None:
+    """Return the unique join of two class names in the type system.
+
+    :param psi: The Psi object representing the type system.
+    :param ti: The first class name.
+    :param tj: The second class name.
+    :return: The unique join of the two class names.
+    """
+    join_set = join(psi, ti, tj)
+    if len(join_set) == 1:
+        return next(iter(join_set))
+    else:
+        return None
+
+
 """ Propositions to check the type system
 """
 
