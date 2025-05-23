6d20ad3fa38bf44bfc2181204ac0397351b868cd
Javier Kauer <kauer.javier01@gmail.com>
2025-05-23 17:51:56 -0400

    Feat: exporting same defintions from static system

---
+++ src/gradual/definitions.py
@@ -0,0 +1,15 @@
+from src.static.definitions import Type, TopType, BottomType, FunctionType, ClassName, Edge, Signature, Psi, Specification
+
+""" Exports the classes and functions from the static definitions module. """
+
+__all__ = [
+    "Type",
+    "TopType",
+    "BottomType",
+    "FunctionType",
+    "ClassName",
+    "Edge",
+    "Signature",
+    "Psi",
+    "Specification",
+]
