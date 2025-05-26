7483533cdf0fb0a0954ba6c0bb9eba208178cfdf
Javier Kauer <kauer.javier01@gmail.com>
2025-05-26 10:53:57 -0400

    Feat: imported functions from the static system

---
+++ src/gradual/functions.py
@@ -0,0 +1,25 @@
+from src.static.functions import (
+    get_all_parent_specifications,
+    join,
+    join_unique,
+    lower_set,
+    meet,
+    meet_unique,
+    names,
+    proj,
+    proj_many,
+    upper_set,
+)
+
+__all__ = [
+    "get_all_parent_specifications",
+    "join",
+    "join_unique",
+    "lower_set",
+    "meet",
+    "meet_unique",
+    "names",
+    "proj",
+    "proj_many",
+    "upper_set",
+]
