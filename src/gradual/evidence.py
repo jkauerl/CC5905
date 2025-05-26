5b9efa1bb1a73bbbef7f5265949be73fb738fa21
Javier Kauer <kauer.javier01@gmail.com>
2025-05-26 11:54:16 -0400

    Feat: first implementation of evidence

---
+++ src/gradual/evidence.py
@@ -0,0 +1,18 @@
+from .definitions import Type
+
+
+class Evidence:
+    """A class to represent the evidence of a type in a gradual type system.
+    In this implementation it is done with an interval.
+    """
+
+    def __init__(self, lower_bound: Type, upper_bound: Type):
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
+
+class CompleteEvidence:
+    """A class to represent complete evidence of a type in a gradual type system."""
+
+    def __init__(self, evidences: list[Evidence]):
+        self.evidences = evidences
