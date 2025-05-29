21fc7defe3b380a05e585a75b35a336bb9205b8a
Javier Kauer <kauer.javier01@gmail.com>
2025-05-29 10:28:37 -0400

    Feat: implementation of is_valid_edge

--- src/gradual/validations.py
+++ src/gradual/validations.py
@@ -1,4 +1,4 @@
-from .definitions import Psi, ClassName, Specification
+from .definitions import Psi, ClassName, Specification, Edge, FunctionType
 from .functions import (
     is_minimal_specification,
     is_includes_node,
@@ -19,4 +19,15 @@ def is_node(psi: Psi, node: ClassName) -> bool:
         and is_includes_node(node, spec, psi)
         and exists_all_signatures(psi, node, spec)
         and no_overloading(spec)
+    )
+
+def is_valid_edge(psi: Psi, class_name_1: ClassName, class_name_2: ClassName) -> bool:
+    """Check if the given edge is valid in the Psi object.
+    
+    param psi: The Psi object representing the type system.
+    param edge: The edge to check.
+    return: True if the edge is valid, False otherwise.
+    """
+    return any(
+        edge.source == class_name_1 and edge.target == class_name_2 for edge in psi.Es
     )
