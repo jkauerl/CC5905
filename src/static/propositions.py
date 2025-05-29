7db51483d4d5ea884141e8459c82ddd2ec13f188
Javier Kauer <kauer.javier01@gmail.com>
2025-05-29 10:47:59 -0400

    Refactor: moved acyclic to propositions

--- src/static/propositions.py
+++ src/static/propositions.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from .definitions import (
     BottomType,
     ClassName,
@@ -82,6 +84,61 @@ def no_overloading(s: Specification) -> bool:
     :return: True if there is no overloading, False otherwise.
     """
     return len(s.signatures) == len(set(sig.var for sig in s.signatures))
+
+
+""" Functions to check for cycles in the type system
+"""
+
+
+def build_adjacency_list(psi: Psi) -> dict:
+    """Funtion to build an adjacency list from the given Psi object.
+
+    :param psi: The Psi object representing the type system.
+    :return: A dictionary representing the adjacency list of the graph.
+    """
+    adj = defaultdict(list)
+    for edge in psi.Es:
+        adj[edge.source].append(edge.target)
+    return adj
+
+
+def dfs(node: ClassName, visited: set, stack: set, adj: dict) -> bool:
+    """Perform a depth-first search to detect cycles in the graph.
+
+    :param node: The current node being visited.
+    :param visited: A set of visited nodes.
+    :param stack: A set of nodes in the current path.
+    :param adj: A dictionary representing the adjacency list of the graph.
+
+    :return: True if no cycle is detected, False otherwise.
+    """
+    visited.add(node)
+    stack.add(node)
+    for neighbor in adj[node]:
+        if neighbor not in visited:
+            if not dfs(neighbor, visited, stack, adj):
+                return False
+        elif neighbor in stack:
+            return False
+    stack.remove(node)
+    return True
+
+
+def acyclic(psi: Psi) -> bool:
+    """Check if the given Psi object is acyclic.
+
+    :param psi: The Psi object representing the type system.
+
+    :return: True if the object is acyclic, False otherwise.
+    """
+    visited = set()
+    stack = set()
+    adj = build_adjacency_list(psi)
+    for node in psi.Ns:
+        if node not in visited:
+            if not dfs(node, visited, stack, adj):
+                return False
+    return True
 
 
 """ Propositions to check the type system
