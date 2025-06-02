a03714488bc61db80e107e58b2e483219ea3cb31
Javier Kauer <kauer.javier01@gmail.com>
2025-06-01 21:55:39 -0400

    Feat: implementation of subtyping functions

---
+++ src/gradual/subtyping.py
@@ -0,0 +1,70 @@
+from .definitions import Psi, Type, ClassName, FunctionType, BottomType, TopType, Specification, Unknown
+import src.static.subtyping as static_subtyping
+
+""" Propositions to check the type system
+"""
+
+
+def is_subtype(psi: Psi, t1: Type, t2: Type, visited=None) -> bool:
+    """Check if t1 is a subtype of t2 in the Psi type system.
+    
+    :param psi: The Psi object representing the type system.
+    :param t1: The first type to check.
+    :param t2: The second type to check.
+    :param visited: A set of visited types to avoid cycles.
+    :return: True if t1 is a subtype of t2, False otherwise.
+    """
+    if visited is None:
+        visited = set()
+
+    if (t1, t2) in visited:
+        return False
+    visited.add((t1, t2))
+
+    if t1 == t2:
+        return True
+    
+    if isinstance(t2, TopType):
+        return True
+    
+    if isinstance(t1, BottomType):
+        return True
+
+    if isinstance(t2, Unknown):
+        return True
+
+    if isinstance(t1, Unknown):
+        return True
+
+    # If t1 is a class name, check if t2 is a class name
+    if isinstance(t1, FunctionType) and isinstance(t2, FunctionType):
+        if len(t1.domain) != len(t2.domain):
+            return False
+        domain_check = all(
+            is_subtype(psi, t2_arg, t1_arg, visited)
+            for t1_arg, t2_arg in zip(t1.domain, t2.domain)
+        )
+        codomain_check = is_subtype(psi, t1.codomain, t2.codomain, visited)
+        return domain_check and codomain_check
+
+    if isinstance(t1, ClassName) and isinstance(t2, ClassName):
+        return static_subtyping.is_subtype(psi, t1, t2)
+
+    return False
+
+
+def is_subtype_spec(s: Specification, sp: Specification, psi: Psi) -> bool:
+    """Check if specification s is a subtype of specification sp.
+    
+    :param s: The first specification to check.
+    :param sp: The second specification to check.
+    :param psi: The Psi object representing the type system.
+    :return: True if s is a subtype of sp, False otherwise.
+    """
+    s_dict = {sig.var: sig.type for sig in s.signatures}
+    for sig_p in sp.signatures:
+        if sig_p.var not in s_dict:
+            return False
+        if not is_subtype(psi, s_dict[sig_p.var], sig_p.type):
+            return False
+    return True
