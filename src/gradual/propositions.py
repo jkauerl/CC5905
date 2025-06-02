9df37816f3ffeed2461a1e7a197691902fa97aa6
Javier Kauer <kauer.javier01@gmail.com>
2025-06-01 22:50:28 -0400

    Feat: implementation of the propositions

--- src/gradual/propositions.py
+++ src/gradual/propositions.py
@@ -1,7 +1,34 @@
+from .definitions import Psi, ClassName, Specification
+from .functions import get_all_parent_specifications
 from src.static.propositions import (
     acyclic,
+    includes_node,
+    exists_all_signatures,
+    no_overloading,
 )
+from src.gradual.subtyping import is_subtype_spec
 
 __all__ = [
     "acyclic",
+    "includes_node",
+    "exists_all_signatures",
+    "no_overloading",
 ]
+
+""" Node validation propositions
+"""
+
+
+def minimal_specification(psi: Psi, class_name: ClassName, s: Specification) -> bool:
+    """Check if the given specification is minimal for the given class name.
+    
+    :param psi: The Psi object representing the type system.
+    :param class_name: The class name to check.
+    :param s: The specification to check.
+    :return: True if the specification is minimal, False otherwise.
+    """
+    parent_specs = get_all_parent_specifications(psi, class_name)
+    for sp in parent_specs:
+        if not is_subtype_spec(s, Specification(sp), psi):
+            return False
+    return True
