7fa5c82c0cc1e03ff67e47d87b89a7d55be85e67
Javier Kauer <kauer.javier01@gmail.com>
2025-06-04 16:09:36 -0400

    Refactoring: redone definitions code

--- src/gradual/evidence/definitions.py
+++ src/gradual/evidence/definitions.py
@@ -1,19 +1,32 @@
-from ..definitions import Type
+from src.static.definitions import Type
 
 
-class Evidence:
-    """A class to represent the evidence of a type in a gradual type system.
-    In this implementation it is done with an interval.
-    """
+class Signature:
+    """ Represents a signature in the type system with lower and upper bounds"""
 
-    def __init__(self,var: str, lower_bound: Type, upper_bound: Type):
+    def __init__(self, var: str, lower_bound: Type, upper_bound: Type):
         self.var = var
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
 
 
-class CompleteEvidence:
-    """A class to represent complete evidence of a type in a gradual type system."""
+class Specification:
+    """ Represents a specification in the type system but with lower and upper bounds"""
 
-    def __init__(self, evidence_1: Evidence, evidence_2: Evidence):
-        self.evidences = (evidence_1, evidence_2)
+    def __init__(self, signatures: list[Signature]):        
+        self.signatures = signatures
+
+
+class Evidence:
+    """ Represents a collection of evidences in the type system"""
+
+    def __init__(self, specification_1: Specification, specification_2: Specification):
+        self.specification_1 = specification_1
+        self.specification_2 = specification_2
+
+
+class CompleteEvidence:
+    """ Represents complete evidence in the type system"""
+
+    def __init__(self, evidences: list[Evidence]):
+        self.evidences = evidences
